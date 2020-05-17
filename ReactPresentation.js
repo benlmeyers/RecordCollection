986848970d39e1959c851e3a5af9a67e76f4875e
Ben <benlmeyers@gmail.com>
2020-05-17 15:34:43 -0500

    adding rest of files

---
+++ ReactPresentation.js
@@ -0,0 +1,34 @@
+const RecordCollection = (props) => (
+  <ul>
+    <h2>My Record Collection</h2>
+    {props.records.map((item, index) =>
+    <Record key={index} item={item}/>
+    )}
+  </ul>
+);
+class Record extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      listenedTo: false,
+    };
+    this.onListItemClick = this.onListItemClick.bind(this);
+  }
+
+  onListItemClick() {
+    this.setState({
+      listenedTo: !this.state.listenedTo,
+    });
+  }
+  render () {
+    var style = {
+      textDecoration: this.state.listenedTo ? 'line-through' : 'none',
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick}>{this.props.item}</li>
+    )
+  }
+};
+
+ReactDOM.render(<RecordCollection records={['Johnny Cash', 'Dave Brubeck', 'The Band']}/>, document.getElementById('app'));
